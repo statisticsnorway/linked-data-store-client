e52829ab36200ab50c740e2b2463ce6832c40510
mmj <mmj@ssb.no>
2019-03-12 11:11:12 +0100

    Switched to HashRouter to handle copy/paste url with static pages.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css'
 import 'react-table/react-table.css'
 import 'react-datepicker/dist/react-datepicker.css'
@@ -8,8 +8,8 @@ import 'react-datepicker/dist/react-datepicker.css'
 import App from './App'
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
